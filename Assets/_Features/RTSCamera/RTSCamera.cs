46044c90fbfe7b2edbc0483ecfa24519d556eb70
Kosciach <maciek.olszok.15@wp.pl>
2025-06-18 19:08:37 +0200

    Move and Rot improvements
    Added:
    - Rot acceleration and inertia
    - Move with acceleration and inertia

--- Assets/_Features/RTSCamera/RTSCamera.cs
+++ Assets/_Features/RTSCamera/RTSCamera.cs
@@ -12,14 +12,26 @@ namespace Kosciach.RTSCameraTask.RTSCamera
     {
         private InputManager _inputMgr;
 
+        [BoxGroup("References"), SerializeField] private Camera _camera;
         [BoxGroup("References"), SerializeField] private CinemachineCamera _cineCamera;
         [BoxGroup("References"), SerializeField] private CinemachineOrbitalFollow _cineOrbitFollow;
         [BoxGroup("References"), SerializeField] private Transform _cameraTarget;
         
         [Header("Rot"), HorizontalLine]
-        [BoxGroup("Settings"), SerializeField] private float _rotSpeed = 3;
+        [BoxGroup("Settings"), SerializeField] private float _rotSpeed = 100;
+        [BoxGroup("Settings"), SerializeField] private float _rotAcceleration = 5;
+        [BoxGroup("Settings"), SerializeField] private float _rotInnertia = 3;
         [BoxGroup("Settings"), SerializeField] private bool _flipRotX;
-        [BoxGroup("Settings"), SerializeField] private bool _flipRotY;
+        [BoxGroup("Settings"), SerializeField] private bool _flipRotY = true;
+        [BoxGroup("Settings"), SerializeField] private float _minXRot = 15;
+        [BoxGroup("Settings"), SerializeField] private float _minYRot = 80;
+        
+        [Header("Move"), HorizontalLine]
+        [BoxGroup("Settings"), SerializeField] private float _moveSpeed = 5;
+        [BoxGroup("Settings"), SerializeField] private float _moveAcceleration = 5;
+        [BoxGroup("Settings"), SerializeField] private float _moveInnertia = 3;
+        [BoxGroup("Settings"), SerializeField] private bool _flipMoveX = true;
+        [BoxGroup("Settings"), SerializeField] private bool _flipMoveY = true;
         
         //Input
         private Vector2 _mouseDeltaInput;
@@ -28,16 +40,18 @@ namespace Kosciach.RTSCameraTask.RTSCamera
         private bool _isRMB;
         
         //Rot
-        private float _horizontalRot;
-        private float _verticalRot;
+        private Vector2 _rot;
+        private Vector2 _rotVelocity;
+        private Vector2 _rotTarget;
         
+        //Move
+        private Vector3 _move;
+        private Vector3 _moveVelocity;
+        private Vector3 _moveTarget;
         
         private void Awake()
         {
             _inputMgr = FindFirstObjectByType<InputManager>();
-
-            _horizontalRot = _cineOrbitFollow.HorizontalAxis.Value;
-            _verticalRot = _cineOrbitFollow.VerticalAxis.Value;
             
             _inputMgr.Inputs.Camera.MouseDelta.performed += ReadMouseDeltaInput;
             _inputMgr.Inputs.Camera.MouseDelta.canceled += ReadMouseDeltaInput;
@@ -65,30 +79,77 @@ namespace Kosciach.RTSCameraTask.RTSCamera
         {
             float deltaTime = Mathf.Min(Time.unscaledDeltaTime, 0.033f);
             
-            Move(deltaTime);
             Rotate(deltaTime);
-        }
-
-        private void Move(float p_deltaTime)
-        {
-            
+            Move(deltaTime);
         }
 
         private void Rotate(float p_deltaTime)
         {
-            if (!_isRMB) return;
-            
             Vector2 rotDir = Vector2.zero;
-            rotDir.x = _mouseDeltaInput.x * (_flipRotX ? -1 : 1);
-            rotDir.y = _mouseDeltaInput.y * (_flipRotY ? -1 : 1);
             
-            Vector2 rot = rotDir * _rotSpeed * Time.deltaTime;
+            //Acceleration
+            if (_isRMB)
+            {
+                //Calculate current direction
+                rotDir.x = _mouseDeltaInput.x * (_flipRotX ? -1 : 1);
+                rotDir.y = _mouseDeltaInput.y * (_flipRotY ? -1 : 1);
+                rotDir *= _rotSpeed * p_deltaTime;
+                
+                //Lerp velocity to direction
+                _rotVelocity = Vector2.Lerp(_rotVelocity, rotDir, _rotAcceleration * p_deltaTime);
+            }
+            else //Inertia
+            {
+                _rotVelocity = Vector2.Lerp(_rotVelocity, Vector2.zero, _rotInnertia * p_deltaTime);
+            }
+            
+            //Apply current rot
+            _rot += _rotVelocity;
 
-            _horizontalRot += rot.x;
-            _verticalRot += rot.y;
+            //Clamp current rot
+            _cineOrbitFollow.VerticalAxis.Range = new Vector2(_minXRot, _minYRot);
+            _rot.y = _cineOrbitFollow.VerticalAxis.ClampValue(_rot.y);
             
-            _cineOrbitFollow.HorizontalAxis.Value = _horizontalRot;
-            _cineOrbitFollow.VerticalAxis.Value = _verticalRot;
+            //Apply rot to cinemachine
+            _cineOrbitFollow.HorizontalAxis.Value = _rot.x;
+            _cineOrbitFollow.VerticalAxis.Value = _rot.y;
+            
+            //Rotate camera target for later move
+            _cameraTarget.rotation = Quaternion.Euler(0, _rot.x, 0);
+        }
+        
+        private void Move(float p_deltaTime)
+        {
+            Vector3 moveDir = Vector3.zero;
+            
+            //Acceleration
+            if (_isLMB)
+            {
+                //Set flips
+                int flipX = (_flipMoveX ? -1 : 1);
+                int flipY = (_flipMoveY ? -1 : 1);
+                
+                //Calculate current direction
+                moveDir = (_cameraTarget.forward * _mouseDeltaInput.y * flipY)
+                        + (_cameraTarget.right * _mouseDeltaInput.x * flipX);
+                moveDir *= _moveSpeed * p_deltaTime;
+                
+                //Lerp velocity to direction
+                _moveVelocity = Vector3.Lerp(_moveVelocity, moveDir, _moveAcceleration * p_deltaTime);
+            }
+            else //Inertia
+            {
+                _moveVelocity = Vector3.Lerp(_moveVelocity, Vector3.zero, _moveInnertia * p_deltaTime);
+            }
+            
+            //Apply current move
+            _move += _moveVelocity;
+
+            //Clamp current move
+
+            
+            //Apply move to cinemachine target
+            _cameraTarget.position += _moveVelocity;
         }
 
 #region ReadInputs
